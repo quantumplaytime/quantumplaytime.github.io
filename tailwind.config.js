a5703f9e88b686c603e80d7f69d0ffb9d868b60d
Zephyr <ghostyzephyr@gmail.com>
2024-11-12 08:17:39 -0500

    first deploy
    wooo!

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    './_layouts/**/*.html',
+    './_includes/**/*.html',
+    './*.html',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#your-primary-color',
+        secondary: '#your-secondary-color',
+      },
+    },
+  },
+  plugins: [],
+};
